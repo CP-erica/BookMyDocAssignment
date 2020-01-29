af6456aa73294b924fec5a6f21372eaf6dd82696
User <ericabasnet18@gmail.com>
2020-01-29 09:57:35 +0545

    commit

--- app/src/main/java/com/example/bookmydocassignment/DocReg.java
+++ app/src/main/java/com/example/bookmydocassignment/DocReg.java
@@ -2,13 +2,24 @@ package com.example.bookmydocassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class DocReg extends AppCompatActivity {
+
+    Button btn_register;
+    EditText txtname,txtpass,txtemail,txtphone,txtadd;
+    Spinner spiner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doc_reg);
+
     }
 }
